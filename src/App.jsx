319cd3e074b664cd93840966c4bb93c671e8236e
amitlevi <amitlevi201002@gmail.com>
2025-03-23 13:20:51 +0200

    side nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Chart from './pages/Chart'
+import StackedExample from './SideNav';
 
 export default function App() {
   return (
     <div>
+       <StackedExample />
       <Chart/>
     </div>
   )
