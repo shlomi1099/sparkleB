ca2ed23dafed92f0266d103ae794400d5477a6b6
liron1997 <liron19975@gmail.com>
2025-03-23 15:03:40 +0200

    added leaderboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Chart from './pages/Chart'
+import Leaderboard from "./components/Leaderboard";
 
 export default function App() {
   return (
     <div>
       <Chart/>
+      <Leaderboard/>
     </div>
   )
 }
