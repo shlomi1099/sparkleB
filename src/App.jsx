6db5ec275cb7cb16ac0ada721155cfae3cfd68f1
shlomi <shlomirosenberg1999@gmail.com>
2025-03-23 12:40:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Chart from './pages/Chart'
+
+export default function App() {
+  return (
+    <div>
+      <Chart/>
+    </div>
+  )
+}
---
+++ src/pages/Chart.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { LineChart } from '@mui/x-charts/LineChart';
+export default function Chart() {
+    let [change, setChange] = useState(2);
+
+
+    return (
+        <div style={{width:"100vw"}}>
+            <div style={{ margin: "0 auto" ,width:"95%"}}>
+                <select style={{ width: "100px", padding: "5px" }} onChange={(val)=>setChange(val.target.value)}>
+                    <option  value="3" >today</option>
+                    <option value="4">yesterday</option>
+                    <option value="1">thurday</option>
+                </select>
+                <div style={{width:"100%"}}>
+                    <p>CONVERSATIONS</p>
+                    <LineChart
+                        xAxis={[{ data: [1, change, 3, 5, 8, 10] }]}
+                        series={[
+                            {
+                                data: [change, 6.6, 8, 8.5, 1.5, 5],
+                            },
+                            {
+                                data: [change, 6.6, 7, 10, 1.5, 2],
+                            },
+                        ]}
+                        width={700}
+                        height={300}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
