b62130ca5894e5d986ec092575b5d62ecc75f05a
amitlevi <amitlevi201002@gmail.com>
2025-03-25 08:40:56 +0200

    table amit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,26 +1,61 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from 'react-bootstrap/Navbar'; 
-import Container from 'react-bootstrap/Container';
-export default function Table() {
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function TableUsers() {
+  const flexContainerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: "100%",
+    fontWeight: "bold",
+  };
+
+  const rowStyle = {
+    padding: "20px",
+    height: "50px",
+    borderTop: "1px solid #ccc",
+  };
+
   return (
-    <div>
-        <Navbar className="bg-body-tertiary " style={{padding:"10px"}}>
-      <Container>
-        <Navbar.Brand href="#teammates">teammates</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-             <a href="#assinges converstion"style={{padding:"10px"}}>assinges converstion</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-             <a href="#close converstion">close converstion</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </div>
-  )
+    <Container fluid style={{ padding: 0, margin: 0 }}>
+      {/* כותרת עמודות */}
+      <Row style={{ margin: 0 }}>
+        <Col style={{ backgroundColor: "#f8f8f8", ...rowStyle }}>
+          <div style={flexContainerStyle}>
+            <div>Teammate</div>
+            <div>Assigned</div>
+            <div>Closed</div>
+          </div>
+        </Col>
+      </Row>
+
+      {/* שורה 1 */}
+      <Row style={{ margin: 0 }}>
+        <Col style={{ backgroundColor: "#FFEBFC", ...rowStyle }}>
+          <div style={flexContainerStyle}>
+            <div>
+              &#128100; <i>"amit"</i>
+            </div>
+            <div>29</div>
+            <div>30</div>
+          </div>
+        </Col>
+      </Row>
+
+      {/* שורה 2 */}
+      <Row style={{ margin: 0 }}>
+        <Col style={{ backgroundColor: "#f0f0f0", ...rowStyle }}>
+          <div style={flexContainerStyle}>
+            <div>
+              &#128100; <i>"matan"</i>
+            </div>
+            <div>34</div>
+            <div>30</div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
